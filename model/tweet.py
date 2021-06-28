f52813777539379504296ff917ac0050ba4bcc95
ricepotato <ricepotato40@gmail.com>
2021-06-29 00:15:36 +0900

    feat: tweet model, unitttest

--- model/tweet.py
+++ model/tweet.py
@@ -16,12 +16,11 @@ class TweetDao:
         return True
 
     def get_timeline(self, user_id):
-        with self.db.session_scope as s:
+        with self.db.session_scope() as s:
             rs = s.query(Follow).filter(Follow.user_id == user_id)
             follow_id_list = [item.follow_id for item in rs]
             follow_id_list.append(user_id)
             tweets = s.query(Tweet).filter(Tweet.user_id.in_(follow_id_list))
             return [
-                {"user_id": tweet["user_id"], "tweet": tweet["tweet"]}
-                for tweet in tweets
+                {"user_id": tweet.user_id, "tweet": tweet.tweet} for tweet in tweets
             ]
--- test_db.py
+++ test_db.py
@@ -2,6 +2,7 @@ import os
 import dotenv
 from model.database import Database
 from model.user import UserDao
+from model.tweet import TweetDao
 
 
 dotenv.load_dotenv(verbose=True)
@@ -13,7 +14,39 @@ def setup_function():
         os.remove(db_file)
 
 
-def test_db():
+def test_tweetdao():
+    db = Database()
+    db.create_all()
+
+    user_dao = UserDao(db)
+    tweet_dao = TweetDao(db)
+    user1_id = user_dao.insert_user(
+        {
+            "email": "sukjun40@naver.com",
+            "name": "사공석준",
+            "password": "password1",
+            "profile": "profile1",
+        }
+    )
+    assert user1_id
+    assert tweet_dao.insert_tweet(user1_id, "tweet text")
+
+    user2_id = user_dao.insert_user(
+        {
+            "email": "ricepotato40@gmail.com",
+            "name": "사공석준2",
+            "password": "password2",
+            "profile": "profile2",
+        }
+    )
+    assert user2_id
+    assert tweet_dao.insert_tweet(user2_id, "tweet text2")
+    assert user_dao.insert_follow(user1_id, user2_id)
+    timeline = tweet_dao.get_timeline(user1_id)
+    assert len(timeline) == 2
+
+
+def test_userdao():
     db = Database()
     db.create_all()
 
@@ -27,3 +60,19 @@ def test_db():
         }
     )
     assert user_id
+
+    res = user_dao.get_user_id_and_password("sukjun40@naver.com")
+    assert user_id == res["id"]
+    assert res["password"] == "password1"
+
+    follow_id = user_dao.insert_user(
+        {
+            "email": "ricepotato40@gmail.com",
+            "name": "사공석준2",
+            "password": "password2",
+            "profile": "profile2",
+        }
+    )
+    assert follow_id
+    assert user_dao.insert_follow(user_id, follow_id)
+    assert user_dao.delete_follow(user_id, follow_id)
